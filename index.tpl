a10474cbf2fbef5e139f5243cb39451693cd8418
b-kaxa <nisigin@gmail.com>
2015-01-25 02:18:53 +0900

    making including .tpl able

---
+++ index.tpl
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset = "UTF-8" />
+    <title>電力使用状況グラフ</title>
+  </head>
+  <body>
+    <p>{{search_result}}</p>
+    <form action = "graph.html" method = "GET">
+      <input type="date" name="date">
+      <input type="submit" value="送信">
+    </form>
+
+  </body>
+</html>
--- pi_search.py
+++ pi_search.py
@@ -1,41 +1,34 @@
 # coding:utf-8
-# 正規表現を用いたプログラム
 
+from bottle import route, request, template, run
 import re
 
+# localhost:8080にアクセスしたときに最初に開くページ
+@route('/')
+def index():
+  search_result = str(match(76))
+  return template('index.tpl', search_result = search_result)
+
 # from http://codepad.org/N9hPp78j
+# 円周率を一万桁まで返す関数
 def pi(n=10000):
   a,b,i = 10**n, 10**n, n*8+1
   while i>=3: a,i = (a+b+b)*(i/2)/i, i-2
   return a-b
 
+# 任意の検索結果を返す関数
 def match(text):
-  pattern = pi()
+  pattern = str(pi())
   match_text = text
-
-  # print pattern
-  # print match_text
 
   matchOB = re.search(str(match_text),str(pattern))
 
   if matchOB:
-    print (str(matchOB.group()) + "は小数点第" + str(matchOB.start()+1) + "位から第" + str(matchOB.end()) + "位に最初に出現します")
+    return_text = (str(matchOB.group()) + "は小数点第" + str(matchOB.start()+1) + "位から第" + str(matchOB.end()) + "位に最初に出現します")
+    return return_text
   else:
-    print(str(text) + 'は円周率10000桁の中に存在しません')
+    return_text = ((text) + 'は円周率10000桁の中に存在しません')
+    return return_text
 
 
-# p = re.compile('[0-9]')
-#print p
-
-# m = re.findall('[0-9]',"9098")
-# m = p.match("9098")
-# print m
-# print m.end()
-# print m.start()
-# print m.span()
-
-# m = p.search("76238947569098")
-# print m.group()
-# print m.end()
-# print m.start()
-# print m.span()
+run(host = 'localhost', port = 8080, debug = True, reloader = True)
